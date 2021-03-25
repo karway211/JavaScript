587cec839062e792e764c938bbc61bfbf40c6694
Andrey Karcheuskiy <karway211@gmail.com>
2021-03-25 17:43:34 +0300

    JS-4-sum-of-row fix(index): add number check

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const range = (min, max) =>
-  BigInt(((max - min) + 1) * (min + max) / 2);
+const range = (min, max) => {
 
-document.write(range(2, 356346363636));
-console.log(range(2, 356346363636).toString());
+  return Number.isInteger(min)
+    && Number.isInteger(max)
+    && max > min
+    && max <= Number.MAX_SAFE_INTEGER
+    && min <= Number.MAX_SAFE_INTEGER
+    && BigInt(((max - min) + 1) * (min + max) / 2)
+    || `Проверьте введенные данные:<br/>
+         min < max;<br/>
+         min == integer,<br/>
+         max = integer;<br/>
+         min <= Number.MAX_SAFE_INTEGER;<br/>
+         min <= Number.MAX_SAFE_INTEGER;`
+}
+
+document.write(range(Number.MAX_SAFE_INTEGER - 1, Number.MAX_SAFE_INTEGER));
