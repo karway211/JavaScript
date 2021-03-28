d5b9a99f476154f2e7a5eaba19cd5f91893af3db
Andrey Karcheuskiy <karway211@gmail.com>
2021-03-28 22:21:41 +0300

    JS-4-sum-of-row fix(index): add calculation after checks

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const range = (min, max) => {
-  const result = ((max - min) + 1) * (min + max) / 2;
   return Number.isInteger(min)
     && Number.isInteger(max)
     && max > min
     && max <= Number.MAX_SAFE_INTEGER
     && min <= Number.MAX_SAFE_INTEGER
-    && (result > Number.MAX_SAFE_INTEGER
-      ? BigInt(result)
-      : result)
+    && (((max - min) + 1) * (min + max) / 2)
     || `Проверьте введенные данные:<br/>
          min < max;<br/>
          min == integer,<br/>
